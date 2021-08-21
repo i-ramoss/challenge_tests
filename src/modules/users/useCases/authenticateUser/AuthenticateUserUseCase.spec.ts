9d80e24d875d7a5150ffff9c1ab2cc61777d5e2e
Ian Ramos <ianramossantos@hotmail.com>
2021-08-21 15:32:36 -0300

    test: ensure that a non-existent user, or with wrong password, is not authenticated

--- src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { IncorrectEmailOrPasswordError } from './IncorrectEmailOrPasswordError';
 import { ICreateUserDTO } from './../createUser/ICreateUserDTO';
 import { InMemoryUsersRepository } from './../../repositories/in-memory/InMemoryUsersRepository';
 import { CreateUserUseCase } from './../createUser/CreateUserUseCase';
@@ -29,5 +30,19 @@ describe('Authenticate User', () => {
     expect(user.name).toEqual(userTest.name);
     expect(user.email).toEqual(userTest.email);
     expect(user).not.toHaveProperty('password');
+  })
+
+  it('should not be able to authenticate a non-existent user', () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({ email: userTest.email, password: userTest.password });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
+  })
+
+  it('should not be able to authenticate a user with wrong password', () => {
+    expect(async () => {
+      const user = await createUserUseCase.execute(userTest);
+
+      await authenticateUserUseCase.execute({ email: user.email, password: 'incorrect_test_password' })
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError);
   })
 })
