5f17d54ac08d339f4543a32a97e053580c259f80
Ian Ramos <ianramossantos@hotmail.com>
2021-08-21 16:15:44 -0300

    test: ensure to search a user by id and show their data

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryUsersRepository } from './../../repositories/in-memory/InMemoryUsersRepository';
+import { CreateUserUseCase } from './../createUser/CreateUserUseCase';
+import { ShowUserProfileUseCase } from './ShowUserProfileUseCase';
+import { ICreateUserDTO } from '../createUser/ICreateUserDTO';
+
+let usersRepositoryInMemory: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+
+describe('Show User Profile', () => {
+  const userTest: ICreateUserDTO = {
+    name: 'User Test',
+    email: 'user@test.com.br',
+    password: 'fake_password'
+  }
+
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    showUserProfileUseCase = new ShowUserProfileUseCase(usersRepositoryInMemory);
+  })
+
+  it('should be able to show the data of an user by id', async () => {
+    const user = await createUserUseCase.execute(userTest);
+
+    const userFound = await showUserProfileUseCase.execute(user.id || "");
+
+    expect(userFound).toHaveProperty('id');
+    expect(userFound).toHaveProperty('password');
+    expect(userFound.password).not.toEqual(userTest.password);
+    expect(userFound.name).toEqual(userTest.name);
+    expect(userFound.email).toEqual(userTest.email);
+  })
+})
