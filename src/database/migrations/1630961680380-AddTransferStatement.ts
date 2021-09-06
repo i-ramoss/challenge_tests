89867b69848caf256c4901639f4b6eb2f7aa27f2
Ian Ramos <ianramossantos@hotmail.com>
2021-09-06 19:35:00 -0300

    chore: add transfer statement on statements migration

---
+++ src/database/migrations/1630961680380-AddTransferStatement.ts
@@ -0,0 +1,28 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class AddTransferStatement1630961680380 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn('statements', 'type', new TableColumn({
+      name: 'type',
+      type: 'enum',
+      enum: ['deposit', 'withdraw', 'transfer'],
+    }))
+
+    await queryRunner.addColumn('statements', new TableColumn({
+      name: 'sender_id',
+      type: 'uuid',
+      isNullable: true,
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn('statements', 'type', new TableColumn({
+      name: 'type',
+      type: 'enum',
+      enum: ['deposit', 'withdraw'],
+    }))
+
+    await queryRunner.dropColumn('statements', 'sender_id');
+  }
+}
